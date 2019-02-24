25c9d88b67a19310d2abd6b64fc48a188bcab7cd
Anthony Pilloud <pilloud.anthony@gmail.com>
2019-02-24 14:17:52 +0100

    [func] add js for animate form

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
+.enableSingleRuntimeChunk()
 .setOutputPath('public/build/')
 .setPublicPath('/build')
+.cleanupOutputBeforeBuild()
 
 .addStyleEntry('css/style', './assets/scss/style.scss')
 
@@ -10,7 +12,11 @@ Encore
 
 .createSharedEntry('vendor', './webpack.shared_entry.js')
 
-.cleanupOutputBeforeBuild()
+.configureBabel(function(babelConfig) {
+}, {
+  include_node_modules: ['ribs-core']
+})
+
 .enableSourceMaps(!Encore.isProduction())
 .enableVersioning(Encore.isProduction())
 
